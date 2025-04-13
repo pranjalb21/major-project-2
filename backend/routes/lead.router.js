0303b0a22d146b89d5f374060807b036367d144f
Pranjal Gain <pranjalgain052@gmail.com>
2025-04-13 17:13:08 +0530

    feat: Post, Update, Delete Get Leads APIs created.

--- backend/routes/lead.router.js
+++ backend/routes/lead.router.js
@@ -2,51 +2,181 @@ import leadSchema from "../utils/validators/lead.schema.js";
 import Lead from "../models/lead.models.js";
 import SalesAgent from "../models/salesAgent.models.js";
 import express from "express";
+import mongoose from "mongoose";
+import { z } from "zod";
 
 export const router = express.Router();
 
 // POST endpoints
-router.post("/", async (req, res) => {
-    try {
-        // Validate request body using Zod schema
-        const parsedData = leadSchema.parse(req.body);
+router
+    .post("/", async (req, res) => {
+        try {
+            // Validate request body using Zod schema
+            const parsedData = leadSchema.parse(req.body);
 
-        // Check if salesAgent exists in the database
-        const existingSalesAgent = await SalesAgent.findById(
-            parsedData.salesAgent
-        );
-        if (!existingSalesAgent) {
-            return res.status(404).json({
-                error: "Sales agent with the provided ID not found.",
+            // Check if salesAgent exists in the database
+            const existingSalesAgent = await SalesAgent.findById(
+                parsedData.salesAgent
+            );
+            if (!existingSalesAgent) {
+                return res.status(404).json({
+                    error: "Sales agent with the provided ID not found.",
+                });
+            }
+
+            // Create a new lead
+            const createdLead = await Lead.create(parsedData);
+
+            // Check if Lead creation failed
+            if (!createdLead) {
+                return res
+                    .status(400)
+                    .json({ error: "Unable to post lead info." });
+            }
+
+            // Populate salesAgent details in the new lead
+            const newLead = await createdLead.populate({
+                path: "salesAgent",
+                select: "_id name",
+            });
+
+            res.status(201).json({
+                message: "New Lead created.",
+                data: newLead,
+            });
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                const message = error.errors.map((e) => e.message);
+                // Handle validation errors from Zod
+                return res.status(400).json({ errors: message });
+            }
+            console.error("Error in POST /:", error);
+            res.status(500).json({
+                error: "Internal server error.",
+                details: error.message,
             });
         }
+    })
+    .get("/", async (req, res) => {
+        try {
+            const { salesAgent, status, tags, source } = req.query;
+            let query = {};
 
-        // Create a new lead
-        const createdLead = await Lead.create(parsedData);
+            if (salesAgent) {
+                if (!mongoose.isValidObjectId(salesAgent)) {
+                    return res.status(400).json({ error: "Invalid Lead ID." });
+                }
+                query.salesAgent = salesAgent;
+            }
 
-        // Check if Lead creation failed
-        if (!createdLead) {
-            return res.status(400).json({ error: "Unable to post lead info." });
+            if (status) {
+                query.status = status;
+            }
+
+            if (tags) {
+                query.tags = { $in: tags };
+            }
+
+            if (source) {
+                query.source = source;
+            }
+
+            const agents = await Lead.find(query);
+
+            // Check if any agents matched the query.
+            if (!agents.length) {
+                return res.status(404).json({ error: "Lead not found." });
+            }
+
+            // Return the list of agents.
+            return res.status(200).json({ data: agents });
+        } catch (error) {
+            // Log the error for debugging.
+            console.error("Error in GET /:", error);
+
+            // Return a 500 error if a server error occurs.
+            return res.status(500).json({ error: "Internal server error." });
         }
+    })
+    .put("/:id", async (req, res) => {
+        try {
+            // Validate request body using Zod schema
+            const parsedData = leadSchema.parse(req.body);
+            const { id } = req.params;
 
-        // Populate salesAgent details in the new lead
-        const newLead = await createdLead.populate({
-            path: "salesAgent",
-            select: "_id name",
-        });
+            const existingUser = await Lead.findById(id);
+            if (!existingUser) {
+                return res
+                    .status(404)
+                    .json({ error: "Lead with the provided ID not found." });
+            }
 
-        // Respond with the newly created lead
-        res.status(201).json({ message: "New Lead created.", data: newLead });
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            const message = error.errors.map((e) => e.message);
-            // Handle validation errors from Zod
-            return res.status(400).json({ errors: message });
+            // Check if salesAgent exists in the database
+            const existingSalesAgent = await SalesAgent.findById(
+                parsedData.salesAgent
+            );
+            if (!existingSalesAgent) {
+                return res.status(404).json({
+                    error: "Sales agent with the provided ID not found.",
+                });
+            }
+
+            // Update lead
+            const updatedLead = await Lead.findByIdAndUpdate(id, parsedData);
+
+            // Check if Lead is updated
+            if (!updatedLead) {
+                return res
+                    .status(400)
+                    .json({ error: "Unable to post lead info." });
+            }
+
+            // Populate salesAgent details in the new lead
+            const newLead = await updatedLead.populate({
+                path: "salesAgent",
+                select: "_id name",
+            });
+
+            res.status(201).json({
+                message: "Lead info updated.",
+                data: newLead,
+            });
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                console.log(error.errors);
+
+                const message = error.errors.map((e) => e.message);
+                // Handle validation errors from Zod
+                return res.status(400).json({ errors: message });
+            }
+            console.error("Error in POST /:", error);
+            res.status(500).json({
+                error: "Internal server error.",
+                details: error.message,
+            });
         }
-        console.error("Error in POST /:", error); // Useful for debugging
-        res.status(500).json({
-            error: "Unable to perform this action right now.",
-            details: error.message,
-        });
-    }
-});
+    })
+    .delete("/:id", async (req, res) => {
+        try {
+            const { id } = req.params;
+            const deletedLead = await Lead.findByIdAndDelete(id);
+
+            // Check if Lead is deleted
+            if (!deletedLead) {
+                return res.status(404).json({
+                    error: "Lead with the provided ID not found.",
+                });
+            }
+
+            res.status(200).json({
+                message: "Lead deleted.",
+                data: deletedLead,
+            });
+        } catch (error) {
+            console.error("Error in DELETE /:", error);
+            res.status(500).json({
+                error: "Internal server error.",
+                details: error.message,
+            });
+        }
+    });
