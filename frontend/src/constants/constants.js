6d4b835251a2f061d314b3c224ab72d76d61336a
Pranjal Gain <pranjalgain052@gmail.com>
2025-04-21 04:08:54 +0530

    feat: Update API base URL to production environment and comment out local URL

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,2 +1,3 @@
-export const base_url = `http://localhost:3000/api/v1`;
+// export const base_url = `http://localhost:3000/api/v1`;
+export const base_url = `https://major-project-2-phi.vercel.app/api/v1`;
 export const title = "Anvaya CRM App"
